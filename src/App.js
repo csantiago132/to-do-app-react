b1aa900a2c5569efce67edeac1314157b68a1df2
Carlos Fernandez <36578444+enrique0921@users.noreply.github.com>
2018-03-31 19:00:35 -0500

    adding constructor and state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import ToDo from './components/ToDo/ToDo';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+  }
+
   render() {
     return (
       <div className="App">
