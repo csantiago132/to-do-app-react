cf7d707ecc550ecce0b1752419ccebfaa7efb1a6
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2018-03-31 19:05:31 -0500

    passing props in ToDo component

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -3,7 +3,10 @@ import React from 'react';
 class ToDo extends React.Component {
     render() {
         return (
-            <li> A todo will go here </li>
+            <li>
+                <input type="checkbox" checked={ this.props.isCompleted } />
+                <span>{ this.props.description }</span>
+            </li>
         );
     }
 }
